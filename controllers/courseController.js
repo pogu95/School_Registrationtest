7d0803ad9b52128106b7a8293c552d511bb17fb5
DJAwaits <padjcreeper@gmail.com>
2022-01-28 09:46:15 -0800

    second commit slide 31

---
+++ controllers/courseController.js
@@ -0,0 +1,39 @@
+const {Course} = require('../models');
+const departments = ['Math', 'English', 'History', 'Science', 'PE', 'Computer Science'].sort();
+
+//view all
+module.exports.viewAll = async function(req, res){
+    const courses = await Course.findAll();
+    res.render('course/view_all', {courses});
+};
+
+//profile
+module.exports.viewProfile= async function(req, res){
+    const course = await Course.findByPk(req.params.id);
+    res.render('course/profile', {course})
+};
+//render add form
+
+//add
+
+//render edit form
+module.exports.renderEditForm = async function(req, res){
+    const course = await Course.findByPk(req.params.id);
+    res.render('course/edit', {course, departments});
+};
+//update
+module.exports.updateCourse = async function(req, res){
+    const course = await Course.update({
+        name: req.body.name,
+        department: req.body.department,
+        instructor_name: req.body.instructor_name,
+        description: req.body.description
+    }, {
+        where: {
+            id: req.params.id
+
+        }
+    });
+    res.redirect(`/courses/profile/${req.params.id}`);
+};
+//delete
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const courseController = require('../controllers/courseController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/courses', courseController.viewAll);
+router.get('/courses/profile/:id', courseController.viewProfile);
+router.get('/courses/edit/:id', courseController.renderEditForm);
+router.post('/courses/edit/:id', courseController.updateCourse);
 module.exports = router;
