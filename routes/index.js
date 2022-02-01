cfa767c2c3ee580462dcd54205fddfb9053a7f75
DJAwaits <padjcreeper@gmail.com>
2022-02-01 08:55:56 -0800

    7th commit part 2 slide 27

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const courseController = require('../controllers/courseController.js');
+const studentController = require('../controllers/studentController.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,4 +15,11 @@ router.post('/courses/edit/:id', courseController.updateCourse);
 router.get('/courses/add', courseController.renderAddForm);
 router.post('/courses/add', courseController.addCourse);
 router.get('/courses/delete/:id', courseController.deleteCourse);
+
+
+router.get('/students', studentController.viewAll);
+router.get('/students/profile/:id', studentController.viewProfile);
+router.get('/students/edit/:id', studentController.renderEditForm);
+router.post('/students/edit/:id', studentController.updateStudent);
+router.get('/students/add', studentController.renderAddForm);
 module.exports = router;
