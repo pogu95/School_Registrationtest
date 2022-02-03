e8c502bdb016e9622d6a220ced8fbfc03699d13c
DJAwaits <padjcreeper@gmail.com>
2022-02-03 08:55:28 -0800

    9th commit part 3 slide 21

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,4 +1,4 @@
-const {Student} = require('../models');
+const {Student, Course, StudentCourses} = require('../models');
 
 //view all
 module.exports.viewAll = async function(req, res){
@@ -7,8 +7,17 @@ module.exports.viewAll = async function(req, res){
 };
 //profile
 module.exports.viewProfile = async function(req, res){
-    const student = await Student.findByPk(req.params.id);
-    res.render('student/profile', {student})
+    const student = await Student.findByPk(req.params.id, {
+        include: 'courses'
+    });
+    const courses = await Course.findAll();
+    let availableCourses = [];
+    for (let i=0; i<courses.length; i++){
+        if (!studentHasCourse(student, courses[i])){
+            availableCourses.push(courses[i]);
+        }
+    }
+    res.render('student/profile', {student, availableCourses})
 };
 //render add
 module.exports.renderAddForm = function(req, res) {
@@ -58,3 +67,14 @@ module.exports.updateStudent = async function(req, res){
     });
     res.redirect(`/students/profile/${req.params.id}`);
 };
+
+function studentHasCourse(student, course){
+    for (let i=0; i<student.courses.length; i++){
+        if (course.id === student.courses[i].id){
+            return true
+        }
+    }
+    return false
+}
+
+//Add course to student
